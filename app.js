12ed6be6e5d0db8c12653bdde933e49ca91926a2
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-07-31 09:30:05 -0500

    commiting last weeks changes

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 'use strict';
+
+var myShroom = angular.module('app', []);
+
+console.log("myshroom", myShroom);
+
+myShroom.factory('ShroomFactory', function($q, $http) {
+    function getShrooms() {
+    	console.log("get shrooms running");
+        return $q( (resolve, reject) => {
+            $http.get('./mushroom.json')
+            .then( (shrooms) => {
+            	console.log("shrooms", shrooms);
+                resolve(shrooms);
+            })
+            .catch( (err) => {
+                reject(err);
+            });
+        });
+    }
+    return { getShrooms };
+});
+
+
+myShroom.controller('ShroomController', function($scope, ShroomFactory) {
+    ShroomFactory.getShrooms()
+    .then( (shroomsData) => {
+    	$scope.shroomArr = [];
+        $scope.shroomList = shroomsData.data.mushroom;
+        angular.forEach($scope.shroomList, function(shroomshroom){
+        	$scope.shroomArr.push(shroomshroom);
+        });
+    });
+});
